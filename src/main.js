21f124cd15c2be4a7b4e05a47a457bdfe6efcc40
ah <1846421546@qq.com>
2023-09-16 11:41:16 +0800

    01_项目初始化

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import './plugins/element.js'
+import 'normalize.css'
+import '@/assets/css/base.css'
 Vue.config.productionTip = false
 
 new Vue({
